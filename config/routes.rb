9cf74572840edf39fb46f85205cea1a8cba4acb3
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-02-27 18:19:37 -0500

    Add Canvas controller and set it as index page
    This commit adds a new controller called  to the AwakenCMS project, with an  action that loads data from the  model. The  action is set up as the root page of the application in the  file.
    
    Adresses #13

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # // Author: Bobola Obi
+  # // Date: February 28, 2023
+  # // Description: This adds a new controller called `Canvas` to the AwakenCMS project, with an `index` action that loads data from the `CanvasItem` model.
+  root 'canvas#index'
+
   resources :navbar_views
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
